37d383b5f8a7036882061f9e5f3476023ff61d72
Luis Lopez <metfanforever@gmail.com>
2026-02-03 15:48:23 -0800

    update files for repos

---
+++ img/app.js
@@ -0,0 +1,27 @@
+let name = prompt("What is your name?");
+alert("Welcome, " + name + "! 👋");
+
+let age = prompt("How old are you?");
+if (age < 18) {
+  alert("You're young and full of energy! 🚀");
+} else {
+  alert("Experience is a powerful thing 😎");
+}
+
+let color = prompt("What is your favorite color?");
+if (color.toLowerCase() === "blue") {
+  alert("Blue is calm and cool 💙");
+} else {
+  alert(color + " is a great choice 🎨");
+}
+
+let book = prompt("What is your favorite book?");
+alert("Nice! \"" + book + "\" sounds interesting 📖");
+
+let song = prompt("What is your favorite song?");
+alert("I'll have to check out \"" + song + "\" 🎵");
+
+
+
+
+
